b9b3914bdf2b985bb877bd50dbc3c6ce86aac308
Nobilis <nikolaev-evgeny@yandex.ru>
2021-12-19 00:01:19 +0300

    2021-12-18/23.56 EmployeesBase
    EmployeesRunner. Организовано чтение файлов из папки resources. PeriodCounter()считает количество лет между двумя датами. Генератор случайных дат в работе. Используется класс LocaleDate.

--- src/main/java/employeesBase/EmployeesBase.java
+++
@@ -1,7 +0,0 @@
-public class EmployeesBase {
-
-    public static void main(String[] args) {
-        System.out.println(new PeriodCounter().count());
-    }
-
-}
---
+++ src/main/java/employeesBase/EmployeesList.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmployeesList {
+
+    private List<Employee> employees;
+
+    void initListEmployees() {
+        employees = new ArrayList<>();
+    }
+
+
+}
