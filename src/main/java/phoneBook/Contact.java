7b1726bf2f44ad7556a829477e6fe68f040731cf
Nobilis <nikolaev-evgeny@yandex.ru>
2021-12-21 00:05:30 +0300

    2021-12-20/23.59 Contact.Long parseNumber( String number)

--- src/main/java/phoneBook/Contact.java
+++ src/main/java/phoneBook/Contact.java
@@ -1,25 +1,71 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Contact {
 
     private final String LAST_NAME;
     private final String FIRST_NAME;
     private final String PATRONYMIC;
-    private String mobileOperator;
-    private final String PHONE_NUMBER;
+    private final Long PHONE_NUMBER;
+    private final String typePhone;
+    private String operator;
     private List<Contact> contacts;
 
     public Contact(String lastName,
-                      String firstName,
-                      String patronymic,
-                      String mobileOperator,
-                      String phoneNumber) {
+                   String firstName,
+                   String patronymic,
+                   long phoneNumber,
+                   String typePhone,
+                   String operator) {
         this.LAST_NAME = lastName;
         this.FIRST_NAME = firstName;
         this.PATRONYMIC = patronymic;
-        this.mobileOperator = mobileOperator;
         this.PHONE_NUMBER = phoneNumber;
+        this.typePhone = typePhone;
+        this.operator = operator;
+        contacts = new ArrayList<>();
+    }
+
+    public Contact(String lastName,
+                   String firstName,
+                   String patronymic,
+                   String phoneNumber,
+                   String typePhone,
+                   String operator) {
+        this.LAST_NAME = lastName;
+        this.FIRST_NAME = firstName;
+        this.PATRONYMIC = patronymic;
+        this.PHONE_NUMBER = parseNumber(phoneNumber);
+        this.typePhone = typePhone;
+        this.operator = operator;
+        contacts = new ArrayList<>();
+    }
+
+    public Contact(String lastName,
+                   String firstName,
+                   String patronymic,
+                   long phoneNumber,
+                   String typePhone) {
+        this.LAST_NAME = lastName;
+        this.FIRST_NAME = firstName;
+        this.PATRONYMIC = patronymic;
+        this.PHONE_NUMBER = phoneNumber;
+        this.typePhone = typePhone;
+        contacts = new ArrayList<>();
+    }
+
+    public Contact(String lastName,
+                   String firstName,
+                   String patronymic,
+                   String phoneNumber,
+                   String typePhone) {
+        this.LAST_NAME = lastName;
+        this.FIRST_NAME = firstName;
+        this.PATRONYMIC = patronymic;
+        this.PHONE_NUMBER = parseNumber(phoneNumber);
+        this.typePhone = typePhone;
         contacts = new ArrayList<>();
     }
 
@@ -36,14 +82,14 @@ public class Contact {
     }
 
     public String getMobileOperator() {
-        return mobileOperator;
+        return operator;
     }
 
     public void setMobileOperator(String mobileOperator) {
-        this.mobileOperator = mobileOperator;
+        this.operator = mobileOperator;
     }
 
-    public String getPHONE_NUMBER() {
+    public long getPHONE_NUMBER() {
         return PHONE_NUMBER;
     }
 
@@ -53,5 +99,24 @@ public class Contact {
 
     public void setContact(Contact contact) {
         contacts.add(contact);
+    }
+
+    private Long parseNumber( String number) {
+        Long num = null;
+        number = number.replace(" ", "");
+        number = number.replace("+7", "8");
+        Pattern p = Pattern.compile("\\d+");
+        Matcher m = p.matcher(number);
+        String res = "";
+        while (m.find()) {
+            res += m.group();
+        }
+        try {
+            num = Long.parseLong(res);
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+            System.out.println("\"" + number + "\" - неправильный формат номера");
+        }
+        return num;
     }
 }
