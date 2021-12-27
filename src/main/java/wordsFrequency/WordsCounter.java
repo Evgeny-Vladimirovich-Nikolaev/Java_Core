18b600c397abb68ac748054fc469ae10c298099a
Nobilis <nikolaev-evgeny@yandex.ru>
2021-12-27 11:54:54 +0300

    2021-12-27/11.31 WordsFrequency.
    IDEA replaces

--- src/main/java/wordsFrequency/WordsCounter.java
+++ src/main/java/wordsFrequency/WordsCounter.java
@@ -9,13 +9,13 @@ public class WordsCounter {
     public static void wordsCount(String text) {
         Map<String, Long> counts = Stream.of(text.toLowerCase())
                 .map(t -> t.split("[ .,:;?!/\"]+"))
-                .flatMap(strings -> Arrays.stream(strings))
+                .flatMap(Arrays::stream)
                 .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
                 .entrySet().stream()
                 .sorted(Map.Entry.<String, Long>comparingByValue(Comparator.reverseOrder())
                         .thenComparing(Map.Entry.comparingByKey()))
                 .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e2, LinkedHashMap::new));
-        counts.entrySet().stream().forEach(i -> System.out.println(i));
+        counts.entrySet().forEach(System.out::println);
     }
 
 }
