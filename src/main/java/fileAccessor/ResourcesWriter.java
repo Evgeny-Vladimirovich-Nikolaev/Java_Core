90feffdbfdf6630c14d5f10885d8e1b069a10301
Nobilis <nikolaev-evgeny@yandex.ru>
2022-01-06 21:03:02 +0300

    2021-01-06/21.01 LogRunner и LogExecutor. Рефакторинг.
    Создан утилитный класс ResourcesWriter, в который перенесен метод writeFile из класса Log.

--- src/main/java/fileAccessor/ResourcesWriter.java
+++ src/main/java/fileAccessor/ResourcesWriter.java
@@ -1,2 +1,21 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+
 public class ResourcesWriter {
+
+    private ResourcesWriter() {}
+
+    public static synchronized void writeFile(String path, String text, boolean append) {
+        try {
+            BufferedWriter writer = new BufferedWriter(new java.io.FileWriter(path, append));
+            writer.write(text);
+            writer.flush();
+            writer.close();
+        } catch (IOException err) {
+            System.out.println(LoggerMsg.IO_ERROR.getMsg());
+            err.printStackTrace();
+        }
+    }
+
+
 }
--- src/main/java/loggerRunner/Log.java
+++ src/main/java/loggerRunner/Log.java
@@ -1,5 +1,3 @@
-import java.io.BufferedWriter;
-import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -30,7 +28,7 @@ public class Log {
     public void writeLog() {
         String log = createLogText();
         if (currentLevel >= minLevel) {
-            writeFile(log);
+            ResourcesWriter.writeFile(PATH, log, true);
         } else {
             print(log);
         }
@@ -47,18 +45,6 @@ public class Log {
         LocalDateTime time = LocalDateTime.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
         return time.format(formatter);
-    }
-
-    private synchronized void writeFile(String log) {
-        try {
-            BufferedWriter writer = new BufferedWriter(new java.io.FileWriter(PATH, true));
-            writer.write(log);
-            writer.flush();
-            writer.close();
-        } catch (IOException err) {
-            System.out.println(LoggerMsg.IO_ERROR.getMsg());
-            err.printStackTrace();
-        }
     }
 
     private synchronized void print(String log) {
