634a050cba11e231b757eb66fb6808aadc9e4f96
Nobilis <nikolaev-evgeny@yandex.ru>
2022-01-06 23:05:32 +0300

    2021-01-06/23.05 LineSeparator

--- src/main/java/lineSeparator/LineSeparator.java
+++ src/main/java/lineSeparator/LineSeparator.java
@@ -1,9 +1,20 @@
-import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 
 public class LineSeparator {
 
+    private static final String path = "./src/main/resources/Не позволяй душе лениться.txt";
+
     public static void main(String[] args) {
-        //ArrayList<String>
+        write(ResourcesReader.readByLines(path).stream()
+                    .filter(s -> s.length() != 0)
+                    .map(s -> s + "Заболоцкий")
+                .collect(Collectors.toList()));
+    }
+
+    private static void write(List<String> lines) {
+
     }
 
 }
