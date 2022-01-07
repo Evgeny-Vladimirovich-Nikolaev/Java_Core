b728e962fd1a3ef3bacfe62f936407f6f1e22a88
Nobilis <nikolaev-evgeny@yandex.ru>
2022-01-07 13:11:41 +0300

    2022-01-06/13.13 LineSeparator. Чтение и запись в потоке Runnable.

--- src/main/java/lineSeparator/LineSeparator.java
+++ src/main/java/lineSeparator/LineSeparator.java
@@ -1,16 +1,27 @@
 import static java.lang.Character.isDigit;
 
-public class LineSeparator {
+public class LineSeparator implements Runnable{
 
+    private final String sourcePath;
+    private final String outPath;
+    private final String fileNameExtension;
+    private final String postFix;
     private static int index = 1;
 
-    public static void main(String[] args) {
-        ResourcesReader.readByLines("Не позволяй душе лениться.txt")
-                .stream()
-                .filter(s -> !s.isEmpty() && !isDigit(s.charAt(0)))
-                .map(s -> s + " Н.А. Заболоцкий")
-            .forEach(s -> ResourcesWriter.writeFile
-                    ("./src/main/resources/Заболоцкий_" + index++ + ".txt", s, false));
+    LineSeparator(String sourcePath, String outPath, String fileNameExtension, String postFix) {
+        this.sourcePath = sourcePath;
+        this.outPath = outPath;
+        this.fileNameExtension = fileNameExtension;
+        this.postFix = postFix;
     }
 
+    @Override
+    public void run() {
+        ResourcesReader.readByLines(sourcePath)
+                .stream()
+                .filter(s -> !s.isEmpty() && !isDigit(s.charAt(0)))
+                .map(s -> s + postFix)
+                .forEach(s -> ResourcesWriter.writeFile
+                        (outPath + index++ + fileNameExtension, s, false));
+    }
 }
---
+++ src/main/java/lineSeparator/MainThreadSeparator.java
@@ -0,0 +1,13 @@
+public class MainThreadSeparator {
+
+    private static String sourcePath = "Не позволяй душе лениться.txt";
+    private static String outPath = "./src/main/resources/Заболоцкий_";
+    private static String fileNameExtension = ".txt";
+    private static String postFix = " Н.А. Заболоцкий";
+
+    public static void main(String[] args) {
+        Runnable lineSeparator =  new LineSeparator(sourcePath, outPath, fileNameExtension, postFix);
+        lineSeparator.run();
+    }
+
+}
