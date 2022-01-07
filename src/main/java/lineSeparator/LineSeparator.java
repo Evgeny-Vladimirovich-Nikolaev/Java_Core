6c5e5c0acae5f5eeac641354ea73a63589c293ca
Nobilis <nikolaev-evgeny@yandex.ru>
2022-01-07 11:47:55 +0300

    2022-01-06/11.46 LineSeparator. Чтение и запись в главном потоке.

--- src/main/java/lineSeparator/LineSeparator.java
+++ src/main/java/lineSeparator/LineSeparator.java
@@ -1,20 +1,16 @@
-import java.util.List;
-import java.util.stream.Collectors;
-
+import static java.lang.Character.isDigit;
 
 public class LineSeparator {
 
-    private static final String path = "./src/main/resources/Не позволяй душе лениться.txt";
+    private static int index = 1;
 
     public static void main(String[] args) {
-        write(ResourcesReader.readByLines(path).stream()
-                    .filter(s -> s.length() != 0)
-                    .map(s -> s + "Заболоцкий")
-                .collect(Collectors.toList()));
-    }
-
-    private static void write(List<String> lines) {
-
+        ResourcesReader.readByLines("Не позволяй душе лениться.txt")
+                .stream()
+                .filter(s -> !s.isEmpty() && !isDigit(s.charAt(0)))
+                .map(s -> s + " Н.А. Заболоцкий")
+            .forEach(s -> ResourcesWriter.writeFile
+                    ("./src/main/resources/Заболоцкий_" + index++ + ".txt", s, false));
     }
 
 }
